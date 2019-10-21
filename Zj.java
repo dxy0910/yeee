c95d7f9b8ea9d89626c7f4ff65d7a7a2737c54d6
dxy0910 <55095373+dxy0910@users.noreply.github.com>
2019-10-21 10:50:14 +0800

    Add files via upload

---
+++ Zj.java
@@ -0,0 +1,17 @@
+
+public class Zj {
+	public static void main(String[] args){
+        MyThread2 myThread2=new MyThread2();
+        myThread2.start();
+   while(true){
+     System.out.println("main()方法在运行");
+       }
+      }
+    }
+class MyThread2 extends Thread{
+          public void run(){
+             while(true){
+       System.out.println("MyThread2类的run()方法在运行");
+             }
+             }
+}
